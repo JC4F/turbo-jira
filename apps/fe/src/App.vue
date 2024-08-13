4ed51965396b90044f1fa466c824f3faa5c161aa
jc4f <lequan12122002@gmail.com>
2024-08-13 10:54:16 +0700

    vite env

--- apps/fe/src/App.vue
+++ apps/fe/src/App.vue
@@ -1,6 +1,13 @@
+<!-- eslint-disable eslint-comments/require-description -->
 <script setup lang="ts">
 import { Button } from '@repo/ui'
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  // eslint-disable-next-line no-console
+  console.log(import.meta.env.VITE_BASE_URL)
+})
 </script>
 
 <template>
