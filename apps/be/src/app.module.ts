f823cb693cf1ccb151363ed7b416d7419e839251
jc4f <lequan12122002@gmail.com>
2024-08-15 17:18:02 +0700

    add lib, register main module

--- apps/be/src/app.module.ts
+++ apps/be/src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { validationSchema } from '@/schema';
 import { SharedModule } from '@/shared/shared.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -15,6 +17,9 @@ import { SharedModule } from '@/shared/shared.module';
       //   allowUnknown: false,
       //   abortEarly: false,
       // },
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
     }),
     SharedModule,
   ],
