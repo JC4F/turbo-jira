ead37920857740401ba700894844df6a8521fe4a
jc4f <lequan12122002@gmail.com>
2024-08-15 09:19:36 +0700

    ui add label

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "@ui/components/ui/button";
-export * from "@ui/components/ui/tooltip";
 export * from "@ui/components/ui/input";
+export * from "@ui/components/ui/label";
+export * from "@ui/components/ui/tooltip";
