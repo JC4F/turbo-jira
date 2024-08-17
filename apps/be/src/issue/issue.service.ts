6c5d259920c9b0f30eea205e7dc06832788d53c8
jc4f <lequan12122002@gmail.com>
2024-08-17 09:45:22 +0700

    wip migrate issue

--- apps/be/src/issue/issue.service.ts
+++ apps/be/src/issue/issue.service.ts
@@ -1,11 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { CreateIssueInput } from './dto/create-issue.input';
 import { UpdateIssueInput } from './dto/update-issue.input';
+import { Issue } from '@/issue/entities/issue.entity';
+import {
+  createEntity,
+  deleteEntity,
+  findEntityOrThrow,
+  updateEntity,
+} from '@/common';
+
+const calculateListPosition = async ({
+  projectId,
+  status,
+}: Partial<Issue>): Promise<number> => {
+  const issues = await Issue.find({ where: { projectId, status } });
+
+  const listPositions = issues.map(({ listPosition }) => listPosition);
+
+  if (listPositions.length > 0) {
+    return Math.min(...listPositions) - 1;
+  }
+  return 1;
+};
 
 @Injectable()
 export class IssueService {
-  create(createIssueInput: CreateIssueInput) {
-    return 'This action adds a new issue';
+  async getProjectIssues(projectId: string, searchTerm: string) {
+    let whereSQL = 'issue.projectId = :projectId';
+
+    if (searchTerm) {
+      whereSQL +=
+        ' AND (issue.title ILIKE :searchTerm OR issue.descriptionText ILIKE :searchTerm)';
+    }
+
+    const issues = await Issue.createQueryBuilder('issue')
+      .select()
+      .where(whereSQL, { projectId, searchTerm: `%${searchTerm}%` })
+      .getMany();
+
+    return issues;
+  }
+
+  async getIssueWithUsersAndComments(issueId: number) {
+    const issue = await findEntityOrThrow(Issue, issueId, {
+      relations: ['users', 'comments', 'comments.user'],
+    });
+
+    return issue;
+  }
+
+  async create(createIssueInput: CreateIssueInput) {
+    const listPosition = await calculateListPosition(createIssueInput);
+    const issue = await createEntity(Issue, {
+      ...createIssueInput,
+      listPosition,
+    });
+    return issue;
   }
 
   findAll() {
@@ -16,11 +66,13 @@ export class IssueService {
     return `This action returns a #${id} issue`;
   }
 
-  update(id: number, updateIssueInput: UpdateIssueInput) {
-    return `This action updates a #${id} issue`;
+  async update(id: number, updateIssueInput: UpdateIssueInput) {
+    const issue = await updateEntity(Issue, id, updateIssueInput);
+    return issue;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} issue`;
+  async remove(id: number) {
+    await deleteEntity(Issue, id);
+    return true;
   }
 }
